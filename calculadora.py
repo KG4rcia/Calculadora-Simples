f8a39999100d02d3ce2ce0f9354900a6142d94d1
K4uan <163343832+KG4rcia@users.noreply.github.com>
2025-03-10 17:54:40 -0300

    Update calculadora.py

--- calculadora.py
+++ calculadora.py
@@ -1,29 +1,36 @@
 # Perguntando qual o tipo de operação pro usuário.
+while True:
+    operação = input("Qual o tipo de operação que você quer realizar?: ").strip().lower()
 
-operação = str(input("Qual o tipo de operação que você deseja?: "))
+    if operação == "sair":
+        print("Fechando programa...")
+        break
+    try:
+        if operação == "mais" or operação == "soma" or operação == "adição":
+            pegar_numero = int(input("Qual o primeiro número?: "))
+            pegar_numero2 = int(input("E o segundo?: "))
+            soma_geral = pegar_numero + pegar_numero2
+            print(soma_geral)
 
-if operação == "soma" or "Soma":
-    pegar_numero = int(input("Qual o primeiro número?: "))
-    pegar_numero2 = int(input("E o segundo?: "))
-    soma_geral = pegar_numero + pegar_numero2
-    print(soma_geral)
+        elif operação == "divisão":
+            pegar_numero = int(input("Qual o primeiro número?: "))
+            pegar_numero2 = int(input("E o segundo?: "))
+            divisão_geral = pegar_numero / pegar_numero2
+            print(divisão_geral)
 
-elif operação == "divisão" or "Divisão":
-    pegar_numero = int(input("Qual o primeiro número?: "))
-    pegar_numero2 = int(input("E o segundo?: "))
-    divisão_geral = pegar_numero + pegar_numero2
-    print(divisão_geral)
+        elif operação == "Multiplicação" or operação == "vezes":
+            pegar_numero = int(input("Qual o primeiro número?: "))
+            pegar_numero2 = int(input("E o segundo?: "))
+            multi_geral = pegar_numero * pegar_numero2
+            print(multi_geral)
 
-elif operação == "Multiplicação" or "multiplicação":
-    pegar_numero = int(input("Qual o primeiro número?: "))
-    pegar_numero2 = int(input("E o segundo?: "))
-    multi_geral = pegar_numero + pegar_numero2
-    print(multi_geral)
-
-elif operação == "subtração" or "Subtração" or "menos" or "Menos":
-    pegar_numero = int(input("Qual o primeiro número?: "))
-    pegar_numero2 = int(input("E o segundo?: "))
-    subt_geral = pegar_numero + pegar_numero2
-    print(subt_geral)
-else:
-    print("Não foi informado o tipo de operação.")
+        elif operação == "subtração" or operação == "menos":
+            pegar_numero = int(input("Qual o primeiro número?: "))
+            pegar_numero2 = int(input("E o segundo?: "))
+            subt_geral = pegar_numero - pegar_numero2
+            print(subt_geral)
+        else:
+            print("Não foi informado o tipo de operação.")
+        
+    except ValueError:
+        print("Digite um valor válido.")
